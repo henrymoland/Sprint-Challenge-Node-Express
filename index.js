cab524b733f8107dec9da10632c281cf61b3298d
Henry Moland <henrymoland@gmail.com>
2018-12-08 18:04:08 -0800

    Created put and delete request/Create router with routes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const projectRouter = require('./routers/projectRouter');
 const actionRouter = require('./routers/actionRouter');
 const server = express();
 const PORT = 5000;
 
 server.use(express.json());
+
+//Routes
 server.use('/actions', actionRouter);
+server.use('/actions', projectRouter);
 
 server.listen(PORT, err => {
     console.log(`Server lisening on PORT ${PORT}`);
